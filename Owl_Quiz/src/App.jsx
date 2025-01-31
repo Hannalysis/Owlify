bb89d411cb423be7ea8d69a47c0f9eb144033cab
Hannalysis <172938274+Hannalysis@users.noreply.github.com>
2025-01-31 11:35:50 +0000

    Delete redundant code from the app file

--- Owl_Quiz/src/App.jsx
+++ Owl_Quiz/src/App.jsx
@@ -4,13 +4,6 @@ import OwlContainer from "./Owl-Container/Owl-Container";
 import OwlQuestions from "./Owl-Container/OwlQuestions";
 import Header from "./Header/Header.jsx"
 
-const data = [
-  {question: "What is React?", answer: " React is a JavaScript library used to build user interfaces for web and native applications. "},
-  {question: " What is a component?", answer: " A component is a piece of the UI that has its own logic and appearance."},
-  {question: "How do you know it’s a React component?", answer: "React component names must always start with a capital letter, while HTML tags must be lowercase."},
-]
-
-
 function App() {
   return <>
     <Header />
--- Owl_Quiz/src/Owl-Container/OwlQuestions.jsx
+++ Owl_Quiz/src/Owl-Container/OwlQuestions.jsx
@@ -1,11 +1,7 @@
 import imagePath from '../owl-images/barn-owl.png'
-
 const OwlQuestions = () => {
-    return ( 
-    <div> <img src= {imagePath} alt="My Image" /> </div> 
-        ); 
+    return (
+    <div> <img src= {imagePath} alt="My Image" /> </div>
+        );
     };
     export default OwlQuestions;
-
-
-
